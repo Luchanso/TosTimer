3be51cd5b7be6e070fcb9270e20c291c169d613e
Loutchansky Oleg <oleg@loutchansky.com>
2015-08-11 02:43:09 +0300

    Add link to buttons

--- src/com/luschanso/tos/timer/screens/MainMenu.hx
+++ src/com/luschanso/tos/timer/screens/MainMenu.hx
@@ -1,8 +1,10 @@
 package com.luschanso.tos.timer.screens;
 
 import com.luschanso.tos.timer.controls.MenuItem;
+import com.luschanso.tos.timer.ScreenEvent;
 import openfl.display.Sprite;
 import openfl.events.Event;
+import openfl.events.MouseEvent;
 
 /**
  * ...
@@ -17,16 +19,34 @@ class MainMenu extends Screen
 		this.screenName = "MainMenu";
 		
 		var beginSessionItem = new MenuItem("Начать сессию");
+		beginSessionItem.addEventListener(MouseEvent.CLICK, beginSessionItem_click);
 		
 		var exportItem = new MenuItem("Экспорт");
+		exportItem.addEventListener(MouseEvent.CLICK, exportItem_click);
 		exportItem.y = beginSessionItem.realHeight;
 		
 		var instructionItem = new MenuItem("Инструкция");
+		instructionItem.addEventListener(MouseEvent.CLICK, instructionItem_click);
 		instructionItem.y = exportItem.y + exportItem.realHeight;
 		
 		this.addChild(beginSessionItem);
 		this.addChild(exportItem);
-		this.addChild(instructionItem);
-		// 04e6ce81-8aeb-451d-81fc-cdeb1f49576e TODO: Связать элементы с переходами на другие экраны
+		this.addChild(instructionItem);		
+	}
+	
+	// c2294e1d-65ed-46ba-b4ee-c473ca57a73c TODO: Прописать правильные линки
+	private function instructionItem_click(e:MouseEvent):Void 
+	{
+		this.dispatchEvent(new ScreenEvent(ScreenEvent.CALL_SCREEN_BY_NAME, this, ""));	
+	}
+	
+	private function exportItem_click(e:MouseEvent):Void 
+	{
+		this.dispatchEvent(new ScreenEvent(ScreenEvent.CALL_SCREEN_BY_NAME, this, ""));		
+	}
+	
+	private function beginSessionItem_click(e:MouseEvent):Void 
+	{
+		this.dispatchEvent(new ScreenEvent(ScreenEvent.CALL_SCREEN_BY_NAME, this, ""));
 	}
 }
