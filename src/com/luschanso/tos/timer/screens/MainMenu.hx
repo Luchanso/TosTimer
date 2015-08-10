7aa1f6f0ad106e084da433fd161063e7b3892f23
Loutchansky Oleg <oleg@loutchansky.com>
2015-08-10 22:00:02 +0300

    Add two menu item

--- src/com/luschanso/tos/timer/screens/MainMenu.hx
+++ src/com/luschanso/tos/timer/screens/MainMenu.hx
@@ -16,6 +16,17 @@ class MainMenu extends Screen
 		
 		this.screenName = "MainMenu";
 		
-		this.addChild(new MenuItem("Начать сессию"));
+		var beginSessionItem = new MenuItem("Начать сессию");
+		
+		var exportItem = new MenuItem("Экспорт");
+		exportItem.y = beginSessionItem.realHeight;
+		
+		var instructionItem = new MenuItem("Инструкция");
+		instructionItem.y = exportItem.y + exportItem.realHeight;
+		
+		this.addChild(beginSessionItem);
+		this.addChild(exportItem);
+		this.addChild(instructionItem);
+		// 04e6ce81-8aeb-451d-81fc-cdeb1f49576e TODO: Связать элементы с переходами на другие экраны
 	}
 }
