c01416661c8840a397013fccf1be5ad8aa28505e
Loutchansky Oleg <oleg@loutchansky.com>
2015-08-10 21:32:23 +0300

    Add ScreenEvent

---
+++ src/com/luschanso/tos/timer/ScreenEvent.hx
@@ -0,0 +1,36 @@
+package com.luschanso.tos.timer;
+
+import openfl.events.Event;
+
+/**
+ * ...
+ * @author Loutchasnky Oleg
+ */
+class ScreenEvent extends Event
+{
+	/**
+	 * Вызов другого экрана по имени
+	 */
+	public static var CALL_SCREEN_BY_NAME = "callScreenByName";
+	
+	/**
+	 * 
+	 */
+	public static var SHOW_SCREEN = "showScreen";	
+	
+	/**
+	 * 
+	 */
+	public static var HIDE_SCREEN = "hideScreen";
+	
+	public var calledScreenName:String;
+	public var eventOwner:Screen;
+
+	public function new(type:String, eventOwner:Screen, calledScreenName = "", bubbles:Bool = false, cancelable:Bool = false)
+	{
+		super(type, bubbles, cancelable);
+		
+		this.calledScreenName = calledScreenName;
+	}
+	
+}
