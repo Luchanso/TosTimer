6fbc102cec08068ef0f877bb1c11507ecb1894f0
Loutchansky Oleg <oleg@loutchansky.com>
2015-08-11 04:15:50 +0300

    Add ExportScreen

---
+++ src/com/luschanso/tos/timer/screens/ExportScreen.hx
@@ -0,0 +1,18 @@
+package com.luschanso.tos.timer.screens;
+
+import com.luschanso.tos.timer.Screen;
+
+/**
+ * ...
+ * @author Loutchasnky Oleg
+ */
+class ExportScreen extends Screen
+{
+	// 0e349e20-adfa-4c44-9281-0073cf293331 TODO: Реализовать ExportScreen
+	public function new() 
+	{
+		super();
+		
+	}
+	
+}
