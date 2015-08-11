494841cda4a6fd741834e19260c84ef99636c669
Loutchansky Oleg <oleg@loutchansky.com>
2015-08-11 04:15:43 +0300

    Add EditScreen

---
+++ src/com/luschanso/tos/timer/screens/EditScreen.hx
@@ -0,0 +1,17 @@
+package com.luschanso.tos.timer.screens;
+
+import com.luschanso.tos.timer.Screen;
+
+/**
+ * ...
+ * @author Loutchasnky Oleg
+ */
+class EditScreen extends Screen
+{
+	// 33027fc4-a8e1-4fad-8157-e48ead3e7cd4 TODO: Реализовать EditScreen
+	public function new() 
+	{
+		
+	}
+	
+}
