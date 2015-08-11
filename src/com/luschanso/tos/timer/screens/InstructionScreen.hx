7480d877cd40b6c64809a9c2127d9006a8e84188
Loutchansky Oleg <oleg@loutchansky.com>
2015-08-11 04:15:58 +0300

    Add InstructionScreen

---
+++ src/com/luschanso/tos/timer/screens/InstructionScreen.hx
@@ -0,0 +1,18 @@
+package com.luschanso.tos.timer.screens;
+
+import com.luschanso.tos.timer.Screen;
+
+/**
+ * ...
+ * @author Loutchasnky Oleg
+ */
+class InstructionScreen extends Screen
+{
+	// 522b304f-687d-442d-aca4-77634c620f35 TODO: Реализовать скрин с инструкциями
+	public function new() 
+	{
+		super();
+		
+	}
+	
+}
