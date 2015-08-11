371e28eb9a14492557d330ce7e1c835e6f85d8dd
Loutchansky Oleg <oleg@loutchansky.com>
2015-08-11 04:16:06 +0300

    Add WorkScreen

---
+++ src/com/luschanso/tos/timer/screens/WorkScreen.hx
@@ -0,0 +1,18 @@
+package com.luschanso.tos.timer.screens;
+
+import com.luschanso.tos.timer.Screen;
+
+/**
+ * ...
+ * @author Loutchasnky Oleg
+ */
+class WorkScreen extends Screen
+{
+	// 4c9adfb8-5828-4cd7-b760-60643b9a4222 TODO: Реализовать класс WorkScreen
+	public function new() 
+	{
+		super();
+		
+	}
+	
+}
