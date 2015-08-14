e3999cfb977947cda9d44374ef4a3fe0757b78c4
Loutchansky Oleg <oleg@loutchansky.com>
2015-08-14 21:09:06 +0300

    Add rightArrow

--- src/com/luschanso/tos/timer/controls/MenuItem.hx
+++ src/com/luschanso/tos/timer/controls/MenuItem.hx
@@ -1,6 +1,9 @@
 package com.luschanso.tos.timer.controls;
 
 import com.luschanso.tos.timer.ScreenEvent;
+import format.SVG;
+import openfl.Assets;
+import openfl.display.Bitmap;
 import openfl.display.Sprite;
 import openfl.Lib;
 import openfl.text.TextField;
@@ -15,15 +18,17 @@ import openfl.text.TextFormatAlign;
 class MenuItem extends Sprite
 {
 	// 29cb78b0-3ff3-4869-86f0-24a505d8ab60 Добавить в MenuItem стрелочку вправо
+	public var realHeight(get, null):Float;
+	public var text(get, set):String;
+	
 	static inline var itemHeightPercent	:Float 	= 0.1;
 	static inline var itemWidthPercent	:Float 	= 1;
+	static inline var iconSize			:Float 	= 50;
 	
 	var lable 		:TextField;
 	var itemWidth 	:Float;
 	var itemHeight	:Float;
-	
-	public var realHeight(get, null):Float;
-	public var text(get, set):String;
+	var rightArrow	:SVG;
 
 	public function new(text:String = "")
 	{
@@ -34,11 +39,22 @@ class MenuItem extends Sprite
 		this.itemHeight = MenuItem.itemHeightPercent * Lib.application.window.height;
 		this.itemWidth = MenuItem.itemWidthPercent * Lib.application.window.width;
 		
+		this.addBackground();
 		this.addLable(text);
 		this.addSplitter();
+		this.addRightArrow();
 		
 		this.mouseEnabled = true;
 		this.buttonMode = true;
+	}
+	
+	function addBackground()
+	{
+		var backgroundColor = 0xFFFFFF;
+		
+		graphics.beginFill(backgroundColor);
+		graphics.drawRect(0, 0, itemWidth, itemHeight);
+		graphics.endFill();
 	}
 	
 	function addLable(text:String):Void
@@ -63,7 +79,7 @@ class MenuItem extends Sprite
 		lable.text = text;
 		lable.autoSize = TextFieldAutoSize.LEFT;
 		lable.x = lablePosition.x - lable.width / 2;
-		lable.y = lablePosition.y - lable.height / 2;	
+		lable.y = lablePosition.y - lable.height / 2;
 		lable.selectable = false;
 		
 		this.addChild(lable);		
@@ -79,6 +95,18 @@ class MenuItem extends Sprite
 		this.graphics.lineStyle(1, splitterColor);
 		this.graphics.moveTo(0, itemHeight);
 		this.graphics.lineTo(itemWidth, itemHeight);
+	}
+	
+	function addRightArrow() 
+	{
+		var iconMarginRight = 50;
+		var rightArrowData = Assets.getText("icons/arrow-right.svg");
+		var iconX = itemWidth - iconSize;
+		var iconY = itemHeight / 2 - iconSize / 4;
+		rightArrow = new SVG(rightArrowData);
+	
+		graphics.lineStyle(1, 0x000000);
+		rightArrow.render(this.graphics, iconX, iconY, iconSize, iconSize);
 	}
 	
 	function get_text():String 
