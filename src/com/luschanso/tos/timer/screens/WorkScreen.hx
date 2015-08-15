cbcd004299f66e232e86cd62d3c0ccaf2a8d1c87
Loutchansky Oleg <oleg@loutchansky.com>
2015-08-15 13:49:24 +0300

    Finish header

--- src/com/luschanso/tos/timer/screens/WorkScreen.hx
+++ src/com/luschanso/tos/timer/screens/WorkScreen.hx
@@ -5,6 +5,7 @@ import com.luschanso.tos.timer.controls.EditButton;
 import com.luschanso.tos.timer.Screen;
 import com.luschanso.tos.timer.ScreenEvent;
 import openfl.events.MouseEvent;
+import openfl.Lib;
 
 /**
  * ...
@@ -16,11 +17,14 @@ class WorkScreen extends Screen
 	var editButton : EditButton;
 	
 	// 4c9adfb8-5828-4cd7-b760-60643b9a4222 TODO: Реализовать класс WorkScreen
-	public function new() 
+	public function new()
 	{
 		super();
 		
 		this.addHeader();
+		this.addMainTimer();
+		this.addTimerList();
+		this.addSummaryTimer();
 	}
 	
 	function addHeader() 
@@ -29,18 +33,43 @@ class WorkScreen extends Screen
 		addEditButton();
 	}
 	
+	function addMainTimer() 
+	{
+		
+	}
+	
+	function addTimerList() 
+	{
+		
+	}
+	
+	function addSummaryTimer() 
+	{
+		
+	}
+	
 	function addEditButton() 
 	{
+		var marginTopEditButton = 10;
+		var marginRightEditButton = 15;
+		
 		editButton = new EditButton();
-		editButton.y = 100;
+		editButton.y = marginTopEditButton;
+		editButton.x = Lib.application.window.width - editButton.width - marginRightEditButton;
+		editButton.addEventListener(MouseEvent.CLICK, editButton_click);
 		
 		addChild(editButton);
+	}
+	
+	private function editButton_click(e:MouseEvent):Void 
+	{
+		dispatchEvent(new ScreenEvent(ScreenEvent.CALL_SCREEN_BY_NAME, this, "EditScreen"));
 	}
 	
 	function addBackButton()
 	{
 		var marginLeftBackButton = 10;
-		var marginTopBackButton = 10;
+		var marginTopBackButton = 12;
 		
 		backButton = new BackButton();
 		backButton.x = marginLeftBackButton;
