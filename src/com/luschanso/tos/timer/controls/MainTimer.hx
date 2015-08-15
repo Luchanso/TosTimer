b48519416fc13ac7374a00e62330aca9ab4c9551
Loutchansky Oleg <oleg@loutchansky.com>
2015-08-15 13:50:04 +0300

    Add MainTimer

---
+++ src/com/luschanso/tos/timer/controls/MainTimer.hx
@@ -0,0 +1,17 @@
+package com.luschanso.tos.timer.controls;
+
+import openfl.text.TextField;
+
+/**
+ * ...
+ * @author Loutchasnky Oleg
+ */
+class MainTimer extends TextField
+{
+
+	public function new() 
+	{
+		super();
+	}
+	
+}
