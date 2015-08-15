e889a597e828dc0b1f943c04384b0fb483e70108
Loutchansky Oleg <oleg@loutchansky.com>
2015-08-15 16:57:19 +0300

    Add TimeViewItem

---
+++ src/com/luschanso/tos/timer/controls/TimeViewItem.hx
@@ -0,0 +1,29 @@
+package com.luschanso.tos.timer.controls;
+
+import openfl.display.Sprite;
+
+/**
+ * ...
+ * @author Loutchasnky Oleg
+ */
+class TimeViewItem extends Sprite
+{
+	static inline var itemHeightPercent	:Float 	= 0.1;
+	static inline var itemWidthPercent	:Float 	= 1;
+
+	public function new() 
+	{
+		super();
+		
+		this.fillBackground();
+		
+	}
+	
+	function fillBackground() 
+	{
+		graphics.beginFill(0xFFFFFF);
+		graphics.
+		graphics.endFill();
+	}
+	
+}
