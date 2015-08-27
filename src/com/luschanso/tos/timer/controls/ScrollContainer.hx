4e31c058a0ea3de933b7a0f7646c012f243920b5
Luchanso <oleg@loutchansky.com>
2015-08-27 12:59:43 +0300

    Change bugs on scrollContainer

--- src/com/luschanso/tos/timer/controls/ScrollContainer.hx
+++ src/com/luschanso/tos/timer/controls/ScrollContainer.hx
@@ -1,8 +1,10 @@
 package com.luschanso.tos.timer.controls;
 
 import openfl.display.Sprite;
+import openfl.events.Event;
 import openfl.events.MouseEvent;
 import openfl.geom.Point;
+import openfl.geom.Rectangle;
 
 /**
  * ...
@@ -14,31 +16,56 @@ class ScrollContainer extends Sprite
 	
 	var _listContent		:List<Sprite>;
 	var mouseDownPosition	:Point;
+	var scrollPosition		:Float = 0;
+	var summaryHight 		:Float = 0;
 	
 	static inline var distanceScrollEvent = 5;
 
 	public function new() 
 	{
-		super();
+		super();		
 		
 		_listContent = new List<Sprite>();
 		
+		/*
+			
+			this.addChild(new NewClass());
+			this.graphics.beginFill(0xFF8000, 0.4);
+			this.graphics.drawRect(0, 0, Settings.width, Settings.height - 150);
+
+		*/
+		
 		addEventListener(MouseEvent.MOUSE_DOWN, mouseDown);
-		addEventListener(MouseEvent.MOUSE_UP, mouseUp);
+		addEventListener(MouseEvent.MOUSE_MOVE, mouseMove);
 	}
 	
-	// 8b7bdcf9-6a93-43a3-9fca-73207b0016c2 Реализовать скрол
-	private function mouseUp(e:MouseEvent):Void 
-	{
-		if (Math.abs(e.stageY - mouseDownPosition.y) > distanceScrollEvent) 
+	public function moveToScrollPosition() 
+	{	
+		var summ = 0.0;
+		
+		for (item in this._listContent) 
 		{
-			
+			item.y = scrollPosition + summ;
+			summ += item.height;
 		}
 	}
 	
-	private function mouseDown(e:MouseEvent):Void 
+	// 8b7bdcf9-6a93-43a3-9fca-73207b0016c2 Реализовать скрол
+	function mouseMove(e:MouseEvent):Void
 	{
-		mouseDownPosition = new Point(e.stageX, e.stageY);		
+		if (e.buttonDown)
+		{
+			if (Math.abs(e.stageY - mouseDownPosition.y) > distanceScrollEvent)
+			{
+				scrollPosition = e.stageY - mouseDownPosition.y;
+				moveToScrollPosition();
+			}
+		}
+	}
+	
+	function mouseDown(e:MouseEvent):Void
+	{
+		mouseDownPosition = new Point(e.stageX, e.stageY);
 	}
 	
 	function get_listContent():List<Sprite> 
@@ -47,9 +74,13 @@ class ScrollContainer extends Sprite
 	}
 	
 	public function addContent(sprite:Sprite)
-	{
+	{	
+		sprite.y = summaryHight + scrollPosition;
+		
 		this._listContent.push(sprite);
 		this.addChild(sprite);
+		
+		summaryHight += sprite.height;
 	}
 	
 	public function removeContent(item)
